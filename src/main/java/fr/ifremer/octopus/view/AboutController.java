764acc3e11dd8191194b57c66363f304c7a25bf8
Sophie Bregent <sophie.bregent@altran.com>
2017-06-08 08:41:05 +0000

    add a webview to diaplay roadmap in about tab
    Former-commit-id: 7bb61e54622744a867a5d52bd7ede9f42d24765d

--- src/main/java/fr/ifremer/octopus/view/AboutController.java
+++ src/main/java/fr/ifremer/octopus/view/AboutController.java
@@ -1,21 +1,51 @@
 package fr.ifremer.octopus.view;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.scene.web.WebView;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import fr.ifremer.octopus.MainApp;
+import fr.ifremer.octopus.utils.PreferencesManager;
 
 
 
 public class AboutController {
 	static final Logger LOGGER = LogManager.getLogger(AboutController.class.getName());
-
 	/**
 	 * Reference to the main application
 	 */
 	private MainApp mainApp;
+
+	@FXML
+	private WebView roadmapweb;
+	
+	@FXML
+	private void initialize() {
+		Locale locale = PreferencesManager.getInstance().getLocale();
+		try {
+			String content = new String(Files.readAllBytes(Paths.get(getClass().getResource("/roadmap/roadmap_"+locale.toString()+".txt").toURI())));
+
+			roadmapweb.getEngine().loadContent(content);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+	 
 	/**
 	 * Is called by the main application to give a reference back to itself.
 	 * 

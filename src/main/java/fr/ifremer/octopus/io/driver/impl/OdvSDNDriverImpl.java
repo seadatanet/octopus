d53b4e6a2968f93e94d338eb2e5269eeaf0d0dfe
Sophie Bregent <sophie.bregent@altran.com>
2017-06-14 10:15:53 +0000

    36215: fix UTF8 with BOM read  on windows
    Former-commit-id: a0312e5d95df0253966fa023b96052339f846a45

--- src/main/java/fr/ifremer/octopus/io/driver/impl/OdvSDNDriverImpl.java
+++ src/main/java/fr/ifremer/octopus/io/driver/impl/OdvSDNDriverImpl.java
@@ -1,8 +1,10 @@
 package fr.ifremer.octopus.io.driver.impl;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -19,11 +21,13 @@ public class OdvSDNDriverImpl extends Driver {
 		boolean canOpen = false;
 		BufferedReader reader  = null;
 		try{
+
+
 			reader = new BufferedReader(new FileReader(file)) ;
 			String firstLine = reader.readLine();
 			// TODO BGT specs: define more criteria here?
 			// Actual criteria: if first character is "//", the file is a odv file
-			
+
 			if (firstLine.startsWith(UTF8_BOM)){
 				firstLine = firstLine.substring(1, firstLine.length());
 			}
@@ -41,6 +45,34 @@ public class OdvSDNDriverImpl extends Driver {
 				reader.close();
 			} catch (IOException e) {
 				LOGGER.debug(e.getMessage());
+			}
+		}
+
+		if (!canOpen){
+
+			FileInputStream fis = null;BufferedReader bf = null ;
+			try{
+				fis = new FileInputStream(file);
+				bf = new BufferedReader(new InputStreamReader(fis, "UTF8"));
+				String firstLine = bf.readLine();
+				if (firstLine.startsWith(UTF8_BOM)){
+					firstLine = firstLine.substring(1, firstLine.length());
+				}
+				if( firstLine.trim().startsWith("//")){
+					format = Format.ODV_SDN;
+					canOpen= true;
+				}else{
+					canOpen= false;
+				}
+			}catch(Exception e){
+				LOGGER.error(e.getMessage());
+			}finally{
+				try{
+					fis.close();
+					bf.close();
+				} catch (IOException e) {
+					LOGGER.debug(e.getMessage());
+				}
 			}
 		}
 		return canOpen;

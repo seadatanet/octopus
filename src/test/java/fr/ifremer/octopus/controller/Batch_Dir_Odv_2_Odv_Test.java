dd21e60452c299a67bc977cacd21d4903633b041
Sophie Bregent <sophie.bregent@altran.com>
2017-06-22 13:10:40 +0000

    fix test
    Former-commit-id: 7c31c7f45924f17384428d9ecf74b426b563ee99

--- src/test/java/fr/ifremer/octopus/controller/Batch_Dir_Odv_2_Odv_Test.java
+++ src/test/java/fr/ifremer/octopus/controller/Batch_Dir_Odv_2_Odv_Test.java
@@ -97,13 +97,14 @@ public class Batch_Dir_Odv_2_Odv_Test extends AbstractBatch_X_2_Y_Test {
 	/**
 	 * 1 odv dir with invalid file files ->
 	 */
-	//@Test
+	@Test
 	public void dir_odv_invalid() {
 		inFormat="odv";
 		in="odv/input/profile_invalid";
 		out = "profile/invalid";
 		type= "keep";
 		outFormat = "odv";
+		expectOutputExist=false;
 		launchTest(logger);
 		checkResult(expectOutputExist);
 		resume(in, out, logger);

53d07c0136611d8de0e7b9b83c7abc5186c205e6
Sophie Bregent <sophie.bregent@altran.com>
2018-03-26 14:15:42 +0000

    get ALL BODC lists and Mapping during tests, before build
    Former-commit-id: 5ab36faea8f27ac8732281cbe57366759511299e

--- src/test/java/fr/ifremer/octopus/utils/SDNVocabsTest.java
+++ src/test/java/fr/ifremer/octopus/utils/SDNVocabsTest.java
@@ -34,8 +34,12 @@ public class SDNVocabsTest {
 			
 			// lists
 			String[] vocabsList = {
-					SdnVocabularyManager.LIST_P01, SdnVocabularyManager.LIST_P06, SdnVocabularyManager.LIST_P09,
-					SdnVocabularyManager.LIST_L22, SdnVocabularyManager.LIST_L33};
+					SdnVocabularyManager.LIST_P01, SdnVocabularyManager.LIST_P02, 
+					SdnVocabularyManager.LIST_P06, SdnVocabularyManager.LIST_P09,
+					SdnVocabularyManager.LIST_L22, SdnVocabularyManager.LIST_L33, 
+					SdnVocabularyManager.LIST_L23,
+					SdnVocabularyManager.LIST_C17,	SdnVocabularyManager.LIST_C77,
+					SdnVocabularyManager.LIST_L05};
 		
 			int version;
 			HashMap<String, Integer> oldVersions = new HashMap<>();
@@ -104,14 +108,45 @@ public class SDNVocabsTest {
 			ICollection p02 = SDNVocabs.getInstance().getCf().getCollection(true, "P02");
 			ICollection p01 = SDNVocabs.getInstance().getCf().getCollection(true, "P01");
 			
-			try {
-			ICollectionMapping p06_from_P09 = SDNVocabs.getInstance().getCf().getMapping(true, p06, p09.getMappedDescriptionFromKey(SdnVocabularyManager.LIST_P09));
-			ICollectionMapping p01_from_P09 = SDNVocabs.getInstance().getCf().getMapping(true, p01, p09.getMappedDescriptionFromKey(SdnVocabularyManager.LIST_P09));
-			ICollectionMapping p01_from_P02 = SDNVocabs.getInstance().getCf().getMapping(true, p01, p02.getMappedDescriptionFromKey(SdnVocabularyManager.LIST_P02));
-			} catch (VocabularyException e) {
-				LOGGER.info(e.getMessage()+System.getProperty("line.separator"));
-			}
+//			try {
+//			ICollectionMapping p06_from_P09 = SDNVocabs.getInstance().getCf().getMapping(true, p06, p09.getMappedDescriptionFromKey(SdnVocabularyManager.LIST_P09));
+//			ICollectionMapping p01_from_P09 = SDNVocabs.getInstance().getCf().getMapping(true, p01, p09.getMappedDescriptionFromKey(SdnVocabularyManager.LIST_P09));
+//			ICollectionMapping p01_from_P02 = SDNVocabs.getInstance().getCf().getMapping(true, p01, p02.getMappedDescriptionFromKey(SdnVocabularyManager.LIST_P02));
+//			} catch (VocabularyException e) {
+//				LOGGER.info(e.getMessage()+System.getProperty("line.separator"));
+//			}
+//			
 			
+			try {
+				LOGGER.debug("mapping P06/P09");
+				ICollectionMapping p06_from_P09 = SDNVocabs.getInstance().getCf().getMapping(true, p06, p09.getMappedDescriptionFromKey(SdnVocabularyManager.LIST_P09));
+				LOGGER.info("mapping P06/P09 : ok");
+			
+				LOGGER.debug("mapping P01/P09");
+				ICollectionMapping p01_from_P09 = SDNVocabs.getInstance().getCf().getMapping(true, p01, p09.getMappedDescriptionFromKey(SdnVocabularyManager.LIST_P09));
+				LOGGER.info("mapping P01/P09 : ok");
+				
+				LOGGER.debug("mapping P01/P02");
+				ICollectionMapping p01_from_P02 = SDNVocabs.getInstance().getCf().getMapping(true, p01, p02.getMappedDescriptionFromKey(SdnVocabularyManager.LIST_P02));
+				LOGGER.info("mapping P01/P02 : ok");
+				/*mapping on list itself (xxx from xxx) is needed to detect deprecated xxx */
+				for (String list: vocabsList){
+					ICollection collec = SDNVocabs.getInstance().getCf().getCollection(true, list);
+					LOGGER.debug("mapping "+list+"/"+list);
+					try{
+						ICollectionMapping automapping = SDNVocabs.getInstance().getCf().getMapping(true, collec, collec.getMappedDescriptionFromKey(list));
+						LOGGER.info("mapping "+list+"/"+list+": ok" );
+						LOGGER.debug("mapping "+list+"/"+list+": ok" );
+					}catch(Exception e){
+						LOGGER.warn("mapping "+list+"/"+list + " does not exist. BODC tems deprecation checks will not be possible for this list.");
+						LOGGER.info("mapping "+list+"/"+list + " does not exist. BODC tems deprecation checks will not be possible for this list.");
+					}
+				}
+			} catch (VocabularyException e) {
+				LOGGER.info(e.getMessage());
+				LOGGER.info(e.getMessage());
+			}finally{
+			}
 			
 		}catch(Exception e){
 			LOGGER.error(e.getMessage());
